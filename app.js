0ed53d26268f90592f9670214cab4f7e7b7e1651
Heinrich Tremblay <het083@g.harvard.edu>
2017-12-21 12:03:35 +0800

    minimalist working state

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+res.send("you just sent a GET request, friend");
+});
+
+var server = app.listen( process.env.PORT || 3000, function(){
+  console.log('Listening on port ' + server.address().port);
+});
