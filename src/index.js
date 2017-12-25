1513c238d3486fce52eb9834d43d0561785e4372
Heinrich Tremblay <het083@g.harvard.edu>
2017-12-26 01:21:07 +0800

    added throng cluster

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import app from './app';
+import throng from 'throng';
 
 
-const server = app.listen( process.env.PORT || 3001, function(){
-  console.log('Listening on port ' + server.address().port);
-});
+// workers
+const WORKERS = process.env.WEB_CONCURRENCY || 4;
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
+
+
+
+function start(id) {
+  console.log(`Started worker ${ id }`);
+
+  process.on('SIGTERM', () => {
+    console.log(`Worker ${ id } exiting...`);
+    process.exit();
+  });
+
+  const server = app.listen( process.env.PORT || 3001, function(){
+    console.log('Listening on port ' + server.address().port);
+  });
+}
